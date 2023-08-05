23b79a8fc2c2f9fcda7c2383aad8046fbaa21d2b
Konstantin Zhigaylo <kostyantop8@gmail.com>
2023-08-05 19:15:27 +0300

    feat: no, seriosly, how did he become a teapot?

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,19 @@
 import { Text, HStack } from "@chakra-ui/react";
 import { FaCoffee } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
-function Logo() 
-{
+function Logo() {
+    const navigate = useNavigate();
+    
+    function brew_coffee() {
+        navigate("/coffee");
+    }
+
     return(
         <>
             <HStack>
                 <Text fontSize='xl' fontWeight={700}>  Konstantin Zero</Text>
-                <FaCoffee fontSize='xl' title="relax, and do what you like"/>
+                <FaCoffee fontSize='xl' onClick={brew_coffee} title="relax, and do what you like"/>
             </HStack>
         </>
     )
