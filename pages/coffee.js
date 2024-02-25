691c029e246f9c9256a9e2d1115e442405b90b21
Konstantin Zhigaylo <zero@kostyazero.com>
2024-02-25 10:17:49 +0300

    refactor: make `/coffee` page looks like 404 page

--- pages/coffee.js
+++ pages/coffee.js
@@ -5,10 +5,10 @@ function GreenTea() {
   return (
     <>
       <Container justifyContent="left" maxW={650} marginLeft="auto" marginRight="auto">
-        <Box margin="20px" marginTop="35px" >
-          <Heading fontSize="4xl" fontWeight={700}>418</Heading>
-          <Text fontSize="md">
-            I&apos;m a teapot
+      <Box margin="20px" marginTop="35px" flex={1} marginBottom="30px">
+          <Heading fontSize="6xl" fontWeight={700} w="100%" textAlign="center">418</Heading>
+          <Text fontSize="md" textAlign="center" fontStyle="italic"> 
+            I&apos;m a teapot!
           </Text>
         </Box>
         <Footer />
