5eedda1412de573b0083d9a7d995126175e5e694
Konstantin Zhigaylo <zero@kostyazero.com>
2025-09-22 20:31:51 +0300

    chore(bun): update deps

--- app/coffee/page.tsx
+++ app/coffee/page.tsx
@@ -1,18 +1,15 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Teapot",
-  description: "I'm a teapot!",
+    title: "Teapot",
+    description: "I'm a teapot!",
 };
 
 export default async function Coffee() {
-  return (
-    <>
-      <p className="text-7xl font-bold font-funnel text-center leading-[1.0] mt-7 text-neutral-50">
-        418
-      </p>
-      <p className="text-center italic mb-7 text-neutral-300">I am a teapot!</p>
-    </>
-  );
+    return (
+        <>
+            <p className="text-7xl font-bold font-funnel text-center leading-[1.0] mt-7 text-neutral-50">418</p>
+            <p className="text-center italic mb-7 text-neutral-300">I am a teapot!</p>
+        </>
+    );
 }
-
