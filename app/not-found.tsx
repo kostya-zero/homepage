817dfd1316990b5d4817073f8f3382785e3b7e56
Konstantin Zhigaylo <zero@kostyazero.com>
2025-12-03 14:29:05 +0300

    refactor(not-found): make page static

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,19 @@
 import { Metadata } from "next";
 
+export const dynamic = "force-static";
+
 export const metadata: Metadata = {
-  title: "404",
-  description: "This is a wrong page.",
+    title: "404",
+    description: "This is a wrong page.",
 };
 
 export default function NotFound() {
-  return (
-    <>
-      <p className="text-7xl font-bold font-funnel bg-clip-text  text-center leading-[1.0] mt-7 text-neutral-50">
-        404
-      </p>
-      <p className="text-center italic mb-7 text-neutral-300">
-        You weren&apos;t supposed to be here...
-      </p>
-    </>
-  );
+    return (
+        <>
+            <p className="text-7xl font-bold font-funnel bg-clip-text  text-center leading-[1.0] mt-7 text-neutral-50">
+                404
+            </p>
+            <p className="text-center italic mb-7 text-neutral-300">You weren&apos;t supposed to be here...</p>
+        </>
+    );
 }
