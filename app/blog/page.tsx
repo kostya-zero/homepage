faf3e80b3a1bbfa898f3daea17d0732d2b067d8e
Konstantin Zhigaylo <zero@kostyazero.com>
2025-12-03 09:37:47 +0300

    chore(deps): update dependencies

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,11 +2,13 @@ import Hero from "@/components/blocks/hero";
 import MainContent from "@/components/blocks/maincontent";
 import Text from "@/components/blocks/text";
 
+export const dynamic = "force-static";
+
 export default function Blog() {
-  return (
-    <MainContent>
-      <Hero>Blog</Hero>
-      <Text>This section is still under construction.</Text>
-    </MainContent>
-  );
+    return (
+        <MainContent>
+            <Hero>Blog</Hero>
+            <Text>This section is still under construction.</Text>
+        </MainContent>
+    );
 }
