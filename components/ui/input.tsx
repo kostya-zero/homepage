f60c0dcea330d72c1e3381131bccec76be37becf
Konstantin Zhigaylo <zero@kostyazero.com>
2025-12-09 19:53:50 +0300

    fix(input): bad outline behaivor

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -4,7 +4,7 @@ function Input({ className, ...props }: React.ComponentProps<"input">) {
     return (
         <input
             className={cn(
-                "px-3 py-2 border rounded-lg outline-2 outline-offset-2 border-border bg-background",
+                "px-3 py-2 border rounded-lg focus:outline-2 focus:outline-offset-2 border-border bg-background",
                 className
             )}
             {...props}
