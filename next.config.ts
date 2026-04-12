ab6b4d26dcd0c5cdeff36c3234607cfd13dd2bfd
Konstantin Zhigaylo <zero@kostyazero.com>
2026-04-12 17:31:17 +0300

    fix(auth): 404 on github authorization

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,23 @@
 import createMDX from "@next/mdx";
 import { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
     pageExtensions: ["tsx", "mdx"],
     poweredByHeader: false,
     reactCompiler: true,
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: '/api/:path*'
+            },
+            {
+                source: '/:path*',
+                destination: '/'
+            }
+        ];
+    }
 };
 
 const withMDX = createMDX({});
